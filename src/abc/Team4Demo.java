1bf67a376dec4ef13dffef93c79a966e80205484
Ahmed <Ahmed@DESKTOP-I1FBRI5.lan>
2024-09-12 20:59:38 -0400

    Added Method

--- src/abc/Team4Demo.java
+++ src/abc/Team4Demo.java
@@ -4,5 +4,8 @@ public class Team4Demo {
 	public static void main(String[] args) {
 		System.out.println("Team 4...");
 	}
-
+	public static void team4() {
+		System.out.println("The team 4 is very good");
+		System.out.println("We will work in importants companies");
+}
 }
